301796f630313113b9c37c9870af1a653cf75ba9
Charles Kim <52868068+charleskimbac@users.noreply.github.com>
2024-07-17 14:59:47 -0400

    sync

--- content.js
+++ content.js
@@ -16,6 +16,18 @@ if (document.readyState === "loading") {
 }
 
 function main() {
+    // check if script has already run (for ff, if extension is reenabled)
+    if (document.getElementById("textbox")) {
+        location.reload();
+    }
+
+    // reload page if data was imported, from popup.js
+    chrome.runtime.onMessage.addListener((message) => {
+        if (message === "reload") {
+            location.reload();
+        }
+    });
+
     addTextbox();
     userID = getUserID();
     loadNote();
@@ -33,8 +45,8 @@ function getUserID() {
 }
 
 function loadNote() {
-    chrome.storage.sync.get([userID], (result) => {
-        if (result[userID]) {
+    chrome.storage.sync.get(userID, (result) => {
+        if (result[userID]) { // if saved note exists
             textbox.value = result[userID];
         }
     });
@@ -43,42 +55,61 @@ function loadNote() {
 function saveNoteOnChange() {
     // on textbox input change, update note
     textbox.addEventListener('input', () => {
-        let note = textbox.value;
-        if (note === "") { // remove note if empty
-            chrome.storage.sync.remove(userID);
-            return;
+        try {
+            const note = textbox.value;
+            if (note === "") { // remove note if empty
+                chrome.storage.sync.remove(userID);
+                return;
+            }
+            const saveData = {};
+            saveData[userID] = note;
+            chrome.storage.sync.set(saveData);
+        } catch (error) { // this happens if ext updates while tab is already open
+            const warning = document.getElementById("warning");
+            warning.style.display = "block";
+            alert("SAVE FAILED! SAVE/COPY YOUR NOTE AND RELOAD THE PAGE!");
+            const textbox = document.getElementById("textbox");
+            textbox.setAttribute("readonly", true);
+            textbox.setAttribute("disabled", true);
         }
-        const saveData = {};
-        saveData[userID] = note;
-        chrome.storage.sync.set(saveData);
     });
 }
 
 function addTextbox() {
-    // Select the target div using the id
+    // select profile card div
     const targetDiv = document.getElementById("profile-header-container");
     
     if (targetDiv && targetDiv.classList.contains("section") && targetDiv.classList.contains("profile-header")) {
-        const div = document.createElement("div"); // holds label and txtbox
+        const div = document.createElement("div");
         div.style.marginBottom = "12px"; // center between 2 sections above/below
         
         // this is to make notes label aligned to top left of div instead of bottom left
-        div.style.display = "flex";
         div.style.alignItems = "flex-start";
         
+        // this happens if ext updates while tab is already open
+        const warning = document.createElement("p");
+        warning.id = "warning";
+        warning.textContent = "SAVE FAILED! SAVE/COPY YOUR NOTE AND RELOAD THE PAGE!";
+        warning.style.display = "none";
+        warning.style.color = "red";
+
         // create "notes" label
         const label = document.createElement("label")
         label.textContent = "Notes: ";
         label.style.fontWeight = "bold";
-        label.style.marginRight = "8px"; // add margin to separate label and textbox
+        label.style.marginRight = "8px";
         
         // new textbox element
         textbox = document.createElement("textarea");
+        textbox.id = "textbox";
         textbox.placeholder = "Enter note here";
         textbox.style.backgroundColor = "#e0e0e0";
         textbox.style.width = "100%";
+        textbox.style.height = "60px";
         
+        div.appendChild(warning);
         div.appendChild(label);
+        div.appendChild(document.createElement("br"));
         div.appendChild(textbox);
         
         // insert the textbox after the target div
--- popup.js
+++ popup.js
@@ -4,8 +4,10 @@ if (document.readyState !== "complete") { // usernames not yet loaded in interac
     main();
 }
 
+let storage = {};
+
 async function main() {
-    let storage = await chrome.storage.sync.get();
+    storage = await getStorage();
     console.log(storage);
 
     /* 
@@ -17,47 +19,184 @@ async function main() {
         }, etc.
     ]
     */
-    const data = [];
-    for (const id in storage) {
-        const data0 = {};
-        data0.userID = id;
-        data0.note = storage[id];
-
-        // get username and display name, cant store this in storage initially since it may change
-        const link = "https://users.roblox.com/v1/users/" + id;
-        const response = await fetch(link);
-        const userData = await response.json();
-        data0.username = userData.name;
-        data0.displayName = userData.displayName;
-
-        data.push(data0);
+    const userIDs = [];
+    for (const userID in storage) {
+        userIDs.push(userID);
     }
 
-    data.sort((a, b) => a.displayName.localeCompare(b.displayName));
+    createNotes(userIDs);
 
-    data.forEach((data0) => {
-        createNote(data0);
-    });
+    const importLabel = document.getElementById("importLabel");
+    let isFirefox = false;
+    try { // getBrowserInfo() is only available in FF
+        const isFirefoxInfo = await browser.runtime.getBrowserInfo();
+        isFirefox = isFirefoxInfo.name === "Firefox";
+    } catch (error) {
+        isFirefox = false;
+    }
+    if (!isFirefox) {
+        importLabel.htmlFor = "fileInput";
+    }
 
+
+    //add onActions to elements
     const fileInput = document.getElementById("fileInput");
-    fileInput.onchange = () => {
-        importNotes(fileInput.files[0]);
-    };
+
+
+
+    // PUT THIS INTO IMPORT FUNC !!! REM THE PARAMTER REQ!! MAYBE NEW GLOBALS!! YAY !!
+
+    // hide file input (and use textbox instead) for ff since popup closes if file selector is opened
+    
+    if (isFirefox) {
+        importLabel.onclick = openImportFirefox;
+
+    } else { // chromium
+        const fileInput = document.createElement("input");
+        fileInput.type = "file";
+        fileInput.accept = ".txt";
+        document.body.appendChild(fileInput);
+
+        importLabel.onclick = () => {
+            fileInput.click();
+        };
+
+        fileInput.onchange = () => {
+            importNotes(fileInput.files[0]);
+        };
+    }
+
+
+
+
+
+
 
     const exportLabel = document.getElementById("exportLabel");
-    exportLabel.onclick = () => { 
+    exportLabel.onclick = () => {
         exportNotes(storage);
     };
+
+
+}
+
+// chrome.storage doesnt work in mv2 (for ff compatibility)
+async function getStorage() {
+    return new Promise((resolve) => {
+        chrome.storage.sync.get(null, (result) => { // get all storage
+            resolve(result);
+        });
+    });
+}
+
+// where userIDs is an array of userIDs
+async function createNotes(userIDs) {
+    const userDataPromises = userIDs.map((userID) => getUserDataPromise(userID));
+    const fetchedUserData = await Promise.all(userDataPromises); // "parallel" fetches
+
+    const data = fetchedUserData.map((userData) => {
+        return {
+            "userID": userData.id,
+            "displayName": userData.displayName,
+            "username": userData.name,
+            "note": storage[userData.id]
+        };
+    });
+
+    // sort
+    data.sort((a, b) => {
+        const name1 = a.displayName.toLowerCase();
+        const name2 = b.displayName.toLowerCase();
+        if (name1 < name2) {
+            return -1;
+        }
+        if (name1 > name2) {
+            return 1;
+        }
+        return 0;
+    });
+
+    data.forEach((data0) => {
+        createSingleNote(data0);
+    });
+}
+
+// input file selector doesnt work in FF popup, use textbox instead
+function openImportFirefox() {
+    const exportLabel = document.getElementById("exportLabel");
+    exportLabel.style.display = "none";
+
+    const importLabel = document.getElementById("importLabel");
+    importLabel.style.display = "none";
+
+    const body = document.body;
+    const importTip = document.createElement("p");
+    importTip.innerText = "To import: open the export .txt file,\npaste the data here, and submit.";
+    body.insertBefore(importTip, importLabel);
+
+    body.insertBefore(document.createElement("br"), importLabel);
+
+    const input = document.createElement("input");
+    input.id = "importInput";
+    body.insertBefore(input, importLabel);
+    input.type = "text";
+    input.placeholder = "Enter export file data here";
+    input.style = "margin-right: 4px;";
+
+    const submit = document.createElement("button");
+    submit.innerText = "Submit";
+    body.insertBefore(submit, importLabel.nextSibling);
+}
+
+
+// get username and display name; cant store this in storage initially since it may change
+function getUserDataPromise(userID) {
+    const link = "https://users.roblox.com/v1/users/" + userID;
+    return fetch(link)
+        .then((response) => response.json());
 }
 
 async function importNotes(file) {
     const raw = await file.text();
-    const json = JSON.parse(raw);
 
-    for (const id in json) {
-        const note = json[id];
-        createNote(id, note);
+    let importedData;
+    try {
+        importedData = JSON.parse(raw);
+    } catch (error) {
+        alert("Invalid file format!");
+        return;
     }
+
+    const userIDs = [];
+    for (const userID in importedData) {
+        userIDs.push(userID);
+
+        // save to storage
+        const importedNote = importedData[userID];
+        // add date before imported note
+        const date = new Date();
+        const month = date.toLocaleString("default", { month: "short" });
+        const day = date.getDate();
+        const year = date.getFullYear();
+        const importDateHeader = "Imported on " + month + " " + day + ", " + year;
+        const importData = importDateHeader + "\n" + importedNote;
+
+        chrome.storage.sync.get([userID], (result) => {
+            const existingNote = result[userID];
+            const saveData = {};
+            if (existingNote) { // if saved note alr exists
+                saveData[userID] = existingNote + "\n\n" + importData;
+                chrome.storage.sync.set(saveData);
+            } else {
+                saveData[userID] = importData;
+                chrome.storage.sync.set(saveData);
+            }
+        });
+    }
+
+    // reload tabs to show new data and so old data cant overwrite
+    chrome.tabs.sendMessage(undefined, "reload");
+    location.reload(); // reload popup
 }
 
 function exportNotes(storage) {
@@ -67,22 +206,22 @@ function exportNotes(storage) {
 
     const a = document.createElement("a");
     a.href = url;
-    a.download = "RPNexport.json";
+    a.download = "RPNexport.txt"; // txt so easily openable for ff import
     a.click();
 }
 
 
-async function createNote(data0) {
+async function createSingleNote(data0) {
     const displayName = data0.displayName;
     const username = data0.username;
-    const id = data0.userID;
+    const userID = data0.userID;
     const note = data0.note;
 
     const div = document.createElement("fieldset");
     div.className = "note";
 
     const p = document.createElement("p");
-    p.id = id + "Note";
+    p.userID = userID + "Note";
     p.textContent = note;
     p.style.display = "none";
 
